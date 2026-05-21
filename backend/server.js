afa2cfb29fda11b800ce6e9a5525054a74a09a03
Raissa Queiroz <raissalqueiroz@gmail.com>
2026-05-21 20:55:21 -0300

    rota clientes admin

--- backend/server.js
+++ backend/server.js
@@ -87,6 +87,20 @@ Agora você receberá promoções exclusivas 🛒`
         }
     );
 });
+app.get('/clientes', (req, res) => {
+    const sql = 'SELECT * FROM clientes ORDER BY data_cadastro DESC';
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                erro: 'Erro ao buscar clientes'
+            });
+        }
+
+        res.json(results);
+    });
+});
 
 app.listen(3000, () => {
 
